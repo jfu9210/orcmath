a2db68b45595e6ab3cb898c8e3e326835c145006
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-01-03 11:17:23 -0500

    gui done

--- OrcMathGUI/src/myStuff/SimonGameJenny.java
+++ OrcMathGUI/src/myStuff/SimonGameJenny.java
@@ -6,14 +6,18 @@ public class SimonGameJenny extends GUIApplication {
 
 	public SimonGameJenny(int width, int height) {
 		super(width, height);
+		setVisible(true);
 	}
 
 	public void initScreen() {
-
+		SimonScreenJenny screen = new SimonScreenJenny(getWidth(), getHeight());
+		setScreen(screen);
 	}
 
 	public static void main(String[] args) {
-
+		SimonGameJenny gui = new SimonGameJenny(800, 550);
+		Thread go = new Thread(gui);
+		go.start();
 	}
 
 }
